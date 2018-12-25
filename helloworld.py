45803eeefcda30c71c889b05f9b7178df53bf600
Shihfan Yang <pc0808f@gmail.com>
2018-12-25 23:29:22 +0800

    helloworld,qtquick

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+import sys
+from PySide2.QtWidgets import QApplication, QLabel
+
+app = QApplication(sys.argv)
+#label = QLabel("Hello World!")
+label = QLabel("<font color=blue size=40>Hello World!</font>")
+label.show()
+app.exec_()
